0983a028b0b379f7fba6f2fad5442df30028d61a
portilho13 <junior.portilho2005@gmail.com>
2024-04-30 18:49:27 +0100

    basic func to get func params

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/portilho13/gocompiler/lexer"
@@ -21,15 +22,84 @@ type Parser struct {
 	index int
 }
 
+type FuncDeclaration struct {
+	FuncName string
+	Params []string
+}
+
 type nt struct {
 	Type string
 	data *lexer.Token
 	children []*nt	
 }
 
+func get_t() (lexer.Token, error) {
+	if len(parser.tokens) - 1 < parser.index {
+		return lexer.Token{}, errors.New("no more tokens")
+	}
+	temp := parser.tokens[parser.index]
+	parser.index++
+	return temp, nil
+}
+
+func unget_t() (lexer.Token, error) {
+	if len(parser.tokens) > parser.index {
+		return lexer.Token{}, errors.New("no more tokens")
+	}
+	temp := parser.tokens[parser.index]
+	parser.index--
+	return temp, nil
+}
+
+func get_args() ([]string, error) {
+	var args []string
+	t, err := get_t()
+	for t.Value != ")" || len(parser.tokens) - 1 < parser.index {
+		if len(parser.tokens) - 1 < parser.index {
+			return nil, errors.New("expected )")
+		}
+		args = append(args, t.Value)
+		t, err = get_t()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return args, nil
+}
+
 
 func Parse() error {
 	parser = &Parser{lexer.GetTokens(), 0}
+	for len(parser.tokens) > parser.index {
+		t, err := get_t()
+		if err != nil {
+			return err
+		}
+		var res []string
+		switch {
+			case t.Type == lexer.KEYWORD:
+				t, err = get_t()
+				if err != nil {
+					return err
+				}
+				res = append(res, t.Value)
+				if t.Type == lexer.IDENTIFIER || t.Type == lexer.LITERAL {
+					t, err = get_t()
+					if err != nil {
+						return err
+					}
+					if (t.Type == lexer.DELIMITER && t.Value == "(") {
+						args, err := get_args()
+						if err != nil {
+							return err
+						}
+						fmt.Println("Funcao: ", res[0])
+						fmt.Println(args)
+					}
+				}
+
+		}
+	}
 	fmt.Println(parser.tokens)
 
     return nil
--- test.c
+++ test.c
@@ -3,3 +3,7 @@ int main() {
     1 + 5;
     return 0;
 }
+
+int rei(int i) {
+    return 0;
+}
